941b724d5151806601a38222dd4b753b8b93b3d1
Gaurav Chatterjee <chatterjeegaur@gmail.com>
2026-01-04 15:49:08 -0500

    most fetch functionality working

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
 import "dotenv/config";
 import express from "express";
+import Client from "./nyrr_api_client/client/index.js";
 
 const PORT = process.env.PORT;
 const APP = express();
 
 APP.get("/health", (_, res) => res.send());
+
+APP.get("/test0", async (_, res) => {
+  const client = new Client();
+  res.send(await client.getRecentEvents());
+});
+
+APP.get("/test1", async (_, res) => {
+  const client = new Client();
+  res.send(await client.getTeamRunners("M2025", "NBR"));
+});
+
+APP.get("/test2", async (_, res) => {
+  const client = new Client();
+  res.send(await client.getAllRaceAwardRunners("M2025", "NBR"));
+});
 
 APP.listen(PORT, () => console.log(`listening on port ${PORT}`));
---
+++ src/nyrr_api_client/instance/index.ts
@@ -0,0 +1,7 @@
+import axios, { type CreateAxiosDefaults } from "axios";
+import https from "https";
+
+const config: CreateAxiosDefaults = { baseURL: `${process.env.API_URL}` };
+if (process.env.ENVIRONMENT === "dev")
+  config.httpsAgent = new https.Agent({ rejectUnauthorized: true });
+export default axios.create(config);
