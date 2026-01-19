fee4df396fe82c2e916679d1c08800bf6967906a
Gaurav Chatterjee <chatterjeegaur@gmail.com>
2026-01-19 16:03:36 -0500

    add logging during initialization

--- src/nyrr_api_client/instance/index.ts
+++ src/nyrr_api_client/instance/index.ts
@@ -1,7 +1,12 @@
 import axios, { type CreateAxiosDefaults } from "axios";
 import https from "https";
 
+console.log("configuring nyrr api client instance...");
 const config: CreateAxiosDefaults = { baseURL: `${process.env.NYRR_API_URL}` };
 if (process.env.ENVIRONMENT === "dev")
   config.httpsAgent = new https.Agent({ rejectUnauthorized: true });
+console.log("configured nyrr api client instance!", {
+  baseURL: config.baseURL,
+  environment: process.env.ENVIRONMENT,
+});
 export default axios.create(config);
