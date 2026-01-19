9bef0f9f4621dd2b045fa877ce170b4c2472a77e
Gaurav Chatterjee <chatterjeegaur@gmail.com>
2026-01-19 15:43:48 -0500

    fix url

--- src/nyrr_api_client/instance/index.ts
+++ src/nyrr_api_client/instance/index.ts
@@ -1,7 +1,7 @@
 import axios, { type CreateAxiosDefaults } from "axios";
 import https from "https";
 
-const config: CreateAxiosDefaults = { baseURL: `${process.env.API_URL}` };
+const config: CreateAxiosDefaults = { baseURL: `${process.env.NYRR_API_URL}` };
 if (process.env.ENVIRONMENT === "dev")
   config.httpsAgent = new https.Agent({ rejectUnauthorized: true });
 export default axios.create(config);
