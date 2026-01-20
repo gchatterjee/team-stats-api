ebb0b36df46749707c103a5bc59168ad41e14c97
Gaurav Chatterjee <chatterjeegaur@gmail.com>
2026-01-20 16:28:39 -0500

    add batching

--- src/scheduled-tasks/team-stats-load-task/index.ts
+++ src/scheduled-tasks/team-stats-load-task/index.ts
@@ -1,12 +1,20 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+  DeleteObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "@aws-sdk/client-s3";
 import NyrrClient from "../../nyrr_api_client/client/index.js";
 import { listObjects } from "../../s3_utils/list-objects.js";
 import type {
   ApiResponse,
   Event,
   RunnerRace,
+  TeamAward,
+  TeamAwardRunner,
+  TeamRunner,
 } from "../../nyrr_api_client/types.js";
 import { DELAY_INCREMENT_MS, promiseAllDelayed } from "../../utils.js";
+import { getObject } from "../../s3_utils/get-object.js";
 
 const s3Client = new S3Client({});
 const BUCKET_NAME = `${process.env.TEAMSTATS_BUCKET_NAME}`;
@@ -14,12 +22,29 @@ const BUCKET_NAME = `${process.env.TEAMSTATS_BUCKET_NAME}`;
 const TEAM_CODE = `${process.env.TEAM_CODE}`;
 
 const JSON_EXTENSION = ".json";
+const IN_PROGRESS_EXTENSION = ".temp.json";
+
+const MAX_RUNNERS = 80;
+
+export type AugmentedRunnerRace = RunnerRace & {
+  teamCode: string | null;
+};
+
+export type Document = {
+  eventCode: string;
+  document: {
+    event: Event;
+    results: ApiResponse<TeamRunner>;
+    awards: (TeamAward | { runners: TeamAwardRunner[] })[];
+    runners: Record<number, AugmentedRunnerRace[]>;
+  };
+};
 
 const getAugmentedRunner = async (
   nyrrClient: NyrrClient,
   runnerId: number,
   teamCode: string,
-): Promise<(RunnerRace | { teamCode: string | null })[]> => {
+): Promise<AugmentedRunnerRace[]> => {
   const [allRaces, teamRaces] = await Promise.all([
     nyrrClient.getRunnerRaces(runnerId),
     nyrrClient.getRunnerRaces(runnerId, teamCode),
@@ -38,26 +63,49 @@ const assembleDocument = async (
   recentEvents: ApiResponse<Event>,
   eventCode: string,
   teamCode: string,
+  partial?: Document,
 ) => {
-  const event = recentEvents.items.find((item) => item.eventCode === eventCode);
-  const results = await nyrrClient.getTeamRunners(eventCode, TEAM_CODE);
-  const awards = await nyrrClient.getAllRaceAwardRunners(eventCode, TEAM_CODE);
-  const runners: Record<number, (RunnerRace | { teamCode: string | null })[]> =
-    {};
+  let event, results, awards, runners;
+  if (partial?.document !== undefined) {
+    event = partial.document.event;
+    results = partial.document.results;
+    awards = partial.document.awards;
+    runners = partial.document.runners;
+  } else {
+    event = recentEvents.items.find((item) => item.eventCode === eventCode);
+    results = await nyrrClient.getTeamRunners(eventCode, TEAM_CODE);
+    awards = await nyrrClient.getAllRaceAwardRunners(eventCode, TEAM_CODE);
+    runners = {};
+  }
+
+  const runnerIds = new Set(results.items.map(({ runnerId }) => runnerId));
+  let runnerCount = 0;
+  const batch: number[] = [];
+  runnerIds.forEach((runnerId) => {
+    if (runnerCount < MAX_RUNNERS && runners[runnerId] === undefined) {
+      batch.push(runnerId);
+      runnerCount += 1;
+    }
+  });
+  console.log(
+    `processing a batch of ${batch.length} out of a total of ${runnerIds.size} runners...`,
+    { batch },
+  );
   await promiseAllDelayed(
-    results.items.map(
-      ({ runnerId }) =>
-        async () =>
-          (runners[runnerId] = await getAugmentedRunner(
-            nyrrClient,
-            runnerId,
-            teamCode,
-          )),
-    ),
+    batch.map((runnerId) => async () => {
+      runners[runnerId] = await getAugmentedRunner(
+        nyrrClient,
+        runnerId,
+        teamCode,
+      );
+    }),
     DELAY_INCREMENT_MS,
   );
 
-  return { eventCode, document: { event, results, awards, runners } };
+  return {
+    document: { eventCode, document: { event, results, awards, runners } },
+    isComplete: Object.keys(runners).length === runnerIds.size,
+  };
 };
 
 export const handler = async (): Promise<void> => {
@@ -75,40 +123,85 @@ export const handler = async (): Promise<void> => {
     ),
   );
 
-  // determine which events are new and are not in the s3 bucket
-  const newEventCodes: string[] = [];
-  recentEventCodes.forEach((code) => {
-    if (code !== undefined && !existingEventCodes.has(code))
-      newEventCodes.push(code);
-  });
+  // determine if there are any events that are incomplete and need to be finished
+  const inProgressEventCodes = existingEvents
+    .filter((key) => key.endsWith(IN_PROGRESS_EXTENSION))
+    .map((key) => key.slice(0, -IN_PROGRESS_EXTENSION.length));
 
-  const [newEventCode] = newEventCodes;
+  let processEventCode: string | undefined;
+  let partial: Document | undefined = undefined;
+  let isResuming = false;
 
-  if (newEventCode === undefined) {
-    console.log("no new events found. exiting.");
+  if (inProgressEventCodes.length > 0) {
+    console.log("found incomplete events in s3 bucket,", {
+      inProgressEventCodes,
+    });
+    isResuming = true;
+    [processEventCode] = inProgressEventCodes;
+
+    console.log(`resuming work on incomplete event ${processEventCode}...`);
+
+    const existing = await getObject(
+      BUCKET_NAME,
+      `${processEventCode}${IN_PROGRESS_EXTENSION}`,
+      s3Client,
+    );
+    if (existing === null) {
+      console.error(
+        "could not retrieve existing in-progress document from s3! exiting.",
+        { processEventCode },
+      );
+      throw Error();
+    }
+    partial = JSON.parse(existing);
+  } else {
+    // determine which events are new and are not in the s3 bucket
+    const newEventCodes: string[] = [];
+    recentEventCodes.forEach((code) => {
+      if (code !== undefined && !existingEventCodes.has(code))
+        newEventCodes.push(code);
+    });
+
+    console.log("found new events to process,", { newEventCodes });
+
+    [processEventCode] = newEventCodes;
+  }
+
+  if (processEventCode === undefined) {
+    console.log("no events found to process. exiting.");
     return;
   }
 
-  console.log(
-    `found new events. this run will attempt to produce the document for ${newEventCode}`,
-    { newEventCodes },
-  );
-
-  const document = await assembleDocument(
+  const { document, isComplete } = await assembleDocument(
     nyrrClient,
     recentEvents,
-    newEventCode,
+    processEventCode,
     TEAM_CODE,
+    partial,
   );
 
-  const key = `${newEventCode}${JSON_EXTENSION}`;
+  const inProcessKey = `${processEventCode}${IN_PROGRESS_EXTENSION}`;
+  const completeKey = `${processEventCode}${JSON_EXTENSION}`;
+  const writeKey = isComplete ? completeKey : inProcessKey;
+  const deleteKey = isResuming && isComplete ? inProcessKey : null;
   const putCommand = new PutObjectCommand({
     Bucket: BUCKET_NAME,
-    Key: key,
+    Key: isComplete ? completeKey : inProcessKey,
     Body: JSON.stringify(document),
     ContentType: "application/json",
   });
-  console.log("uploading document to s3...", { key });
+  console.log("uploading document to s3...", { key: writeKey });
   await s3Client.send(putCommand);
-  console.log("uploaded document to s3!", { key });
+  console.log("uploaded document to s3!", { key: writeKey });
+
+  if (deleteKey !== null) {
+    // delete the in-progress file
+    const deleteCommand = new DeleteObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: deleteKey,
+    });
+    console.log("deleting in-progress document from s3...", { key: deleteKey });
+    await s3Client.send(deleteCommand);
+    console.log("deleted in-progress document from s3!", { key: deleteKey });
+  }
 };
